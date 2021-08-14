c77d1c8aa9eea85d056d80f0e56bbc24f8c5e18b
Sui146 <37026360+Sui146@users.noreply.github.com>
2021-08-14 22:23:45 +0200

    Create Practice2.java
    Added a Set exercise from MCupic

---
+++ JavaPractice/src/hr/oop/practice/Practice2.java
@@ -0,0 +1,45 @@
+package hr.oop.practice;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prvi pokusaj:");
+		System.out.println("-------------");
+		ispisi(ukloni1(args));
+		System.out.println("Drugi pokusaj:");
+		System.out.println("--------------");
+		ispisi(ukloni2(args));
+		System.out.println("Treci pokusaj:");
+		System.out.println("---------------");
+		ispisi(ukloni3(args));
+	}
+	private static Collection<String> ukloni1(String[] args){
+		return napuniSkup(args, new HashSet<String>());
+	}
+
+	private static Collection<String> ukloni2(String[] args){
+		return napuniSkup(args, new TreeSet<String>());
+	}
+
+	private static Collection<String> ukloni3(String[] args){
+		return napuniSkup(args, new LinkedHashSet<String>());
+	}
+	
+	private static Collection<String> napuniSkup(String[] args, Set<String> stuff){
+		for(String s: args) {
+			stuff.add(s);
+		}
+		return stuff;
+	}
+	public static void ispisi(Collection<String> kolekcija){
+		for(String s: kolekcija)
+			System.out.println(s);
+	}
+}

b256eada54dceaf18f52958887d514cf6ee15a21
Sui146 <37026360+Sui146@users.noreply.github.com>
2021-08-17 13:44:21 +0200

    Lambda Practice
     Program gets a List of Doubles, then squares them. And it uses lambda expressions.

--- JavaPractice/src/hr/oop/practice/PracticeLambda.java
+++ JavaPractice/src/hr/oop/practice/PracticeLambda.java
@@ -1,5 +1,20 @@
 package hr.oop.practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PracticeLambda {
+	public static void main(String[] args) {
+		
+		List<Double> arr = new ArrayList<> (
+				Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0));
+		arr.forEach(n->System.out.println(Math.pow(n, 2)));
+		arr.forEach(PracticeLambda::Hello);
+	}
+	public static void Hello(Double n) {
+		Double a = Math.pow(n, 2);
+		System.out.println(n + " " + a );
+	}
 
 }
